d636db4914606588e95f2f4c06d0920eb7ed36aa
Enzo GIOIELLI <enzogio2004@gmail.com>
2026-01-29 10:03:49 +0100

    Add content of sizeof function

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,8 +1,39 @@
+/* Les types à afficher sont les suivants :
+
+i. char
+
+ii. short
+
+iii. int
+
+iv. long int
+
+v. long long int
+
+vi. float
+
+vii. double
+
+viii. long double */
 
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    printf("Hello!");
+    printf("Voici les différentes tailles des types :\n\n");
+    printf("Taille de signed char : %zu octets\n", sizeof(signed char));
+    printf("Taille de unsigned char : %zu octets\n\n", sizeof(unsigned char));
+    printf("Taille de short : %zu octets\n", sizeof(short));
+    printf("Taille de unsigned short : %zu octets\n\n", sizeof(unsigned short));
+    printf("Taille de int : %zu octets\n", sizeof(int));
+    printf("Taille de unsigned int : %zu octets\n\n", sizeof(unsigned int));
+    printf("Taille de long int : %zu octets\n", sizeof(long int));
+    printf("Taille de unsigned long int : %zu octets\n\n", sizeof(unsigned long int));
+    printf("Taille de long long int : %zu octets\n", sizeof(long long int));
+    printf("Taille de unsigned long long int : %zu octets\n\n", sizeof(unsigned long long int));
+    printf("Taille de float : %zu octets\n", sizeof(float));
+    printf("Taille de double : %zu octets\n", sizeof(double));
+    printf("Taille de long double : %zu octets\n", sizeof(long double));
+
+    return 0;
 }
 
