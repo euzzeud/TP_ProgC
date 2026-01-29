544de6879c75c28ef0c38353c8ebe376a21bb012
Enzo GIOIELLI <enzogio2004@gmail.com>
2026-01-29 10:35:24 +0100

    Add boucles function

---
+++ TP1/src/boucles.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* i = numéro de ligne
+j = position dans la ligne */
+
+int main() {
+    int compteur = 5;
+
+    if (compteur < 10 ) {
+
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (j == 1 || j == i || i == compteur) {
+                printf("* ");
+            }
+            else {
+                printf("# ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+} else {
+    printf("La variable 'compteur' n'est pas strictement inférieur à 10. Arrêt du programme.")
+    return 1;
+}
+
+}
